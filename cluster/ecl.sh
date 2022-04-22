53769e505c414c484fe67d822187bdbffd924c86
atulyamenon <atulyamenon6@gmail.com>
2022-04-22 11:23:11 +0200

    Made changes in the run script of ecl

--- cluster/ecl.sh
+++ cluster/ecl.sh
@@ -8,13 +8,13 @@
 export PATH="/home/rosello/anaconda3/bin:$PATH"
 source activate informer-env
 
-srun -u python  main_informer.py --model informer  --checkpoints ${2} --data ECL --data_path . --features M --seq_len 96 --label_len 48 --pred_len 48 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5
-srun -u python  main_informer.py --model informer  --checkpoints ${2} --data ECL --data_path . --features M --seq_len 168 --label_len 168 --pred_len 168 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5
-srun -u python  main_informer.py --model informer  --checkpoints ${2} --data ECL --data_path . --features M --seq_len 168 --label_len 168 --pred_len 336 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5
-srun -u python  main_informer.py --model informer  --checkpoints ${2} --data ECL --data_path . --features M --seq_len 336 --label_len 336 --pred_len 720 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
-srun -u python main_informer.py --model informer  --checkpoints ${2} --data ECL --data_path . --features M --seq_len 336 --label_len 336 --pred_len 960 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
-srun -u python  main_informer.py --model informer  --checkpoints ${2} --data ECL --data_path . --features S --seq_len 720 --label_len 168 --pred_len 48 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
-srun -u python  main_informer.py --model informer  --checkpoints ${2} --data ECL --data_path . --features S --seq_len 720 --label_len 336 --pred_len 168 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
-srun -u python  main_informer.py --model informer  --checkpoints ${2} --data ECL --data_path . --features S --seq_len 720 --label_len 336 --pred_len 336 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
-srun -u python  main_informer.py --model informer  --checkpoints ${2} --data ECL --data_path . --features S --seq_len 720 --label_len 336 --pred_len 720 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
-srun -u python main_informer.py --model informer  --checkpoints ${2} --data ECL --data_path . --features M --seq_len 336 --label_len 336 --pred_len 960 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
+srun -u python  main_informer.py --model informer  --checkpoints ${2}  --data ECL --root_path ./ --features M --seq_len 96 --label_len 48 --pred_len 48 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5
+srun -u python  main_informer.py --model informer  --checkpoints ${2} --data ECL --root_path ./ --features M --seq_len 168 --label_len 168 --pred_len 168 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5
+srun -u python  main_informer.py --model informer  --checkpoints ${2}  --data ECL --root_path ./ --features M --seq_len 168 --label_len 168 --pred_len 336 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5
+srun -u python  main_informer.py --model informer  --checkpoints ${2}  --data ECL --root_path ./ --features M --seq_len 336 --label_len 336 --pred_len 720 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
+srun -u python main_informer.py --model informer  --checkpoints ${2}  --data ECL --root_path ./ --features M --seq_len 336 --label_len 336 --pred_len 960 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
+srun -u python  main_informer.py --model informer  --checkpoints ${2}  --data ECL --root_path ./ --features S --seq_len 720 --label_len 168 --pred_len 48 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
+srun -u python  main_informer.py --model informer  --checkpoints ${2}  --data ECL --root_path ./ --features S --seq_len 720 --label_len 336 --pred_len 168 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
+srun -u python  main_informer.py --model informer  --checkpoints ${2}  --data ECL --root_path ./ --features S --seq_len 720 --label_len 336 --pred_len 336 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
+srun -u python  main_informer.py --model informer  --checkpoints ${2}  --data ECL --root_path ./ --features S --seq_len 720 --label_len 336 --pred_len 720 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
+srun -u python main_informer.py --model informer  --checkpoints ${2}  --data ECL --root_path ./ --features M --seq_len 336 --label_len 336 --pred_len 960 --e_layers 2 --d_layers 1 --attn ${1} --des 'Exp' --itr 5 
