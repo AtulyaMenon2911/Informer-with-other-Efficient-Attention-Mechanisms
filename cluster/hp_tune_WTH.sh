ae255454ad47968ba73140a581e849cd0d59c20e
atulyamenon <atulyamenon6@gmail.com>
2022-05-01 16:17:24 +0200

    Added sh script to run the hyperparameter script

---
+++ cluster/hp_tune_WTH.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=informer_train
+#SBATCH --output=informer_train%j.log
+#SBATCH --error=informer_train%j.err
+#SBATCH --mail-user=rosello@uni-hildesheim.de
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+export PATH="/home/praphul/anaconda3/bin:$PATH"
+source activate informer-env
+
+srun -u python  hp_search.py --model informer  --checkpoints ${1}  --data WTH --root_path ./  --attn qs --des 'Exp' --itr 5 --factor 5 --batch_size 16 --train_epochs 12
+srun -u python  hp_search.py --model informer  --checkpoints ${1} --data WTH --root_path ./   --attn lam --des 'Exp' --itr 5 --factor 5 --batch_size 16 --train_epochs 12
---
+++ cluster/hp_tune_ettm1.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=informer_train
+#SBATCH --output=informer_train%j.log
+#SBATCH --error=informer_train%j.err
+#SBATCH --mail-user=rosello@uni-hildesheim.de
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+export PATH="/home/praphul/anaconda3/bin:$PATH"
+source activate informer-env
+
+srun -u python  hp_search.py --model informer  --checkpoints ${1}  --data ETTm1  --attn qs --des 'Exp' --itr 5 --factor 5 --batch_size 16 --train_epochs 12
+srun -u python  hp_search.py --model informer  --checkpoints ${1} --data ETTm1  --attn lam --des 'Exp' --itr 5 --factor 5 --batch_size 16 --train_epochs 12
