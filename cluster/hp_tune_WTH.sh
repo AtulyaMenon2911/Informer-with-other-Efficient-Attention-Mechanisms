ee9c2213ef8b452886fcb918b1f6a1e9f32ce9aa
atulyamenon <atulyamenon6@gmail.com>
2022-05-01 20:50:16 +0200

    Updated sh script for hp tuning

--- cluster/hp_tune_WTH.sh
+++ cluster/hp_tune_WTH.sh
@@ -8,5 +8,5 @@
 export PATH="/home/praphul/anaconda3/bin:$PATH"
 source activate informer-env
 
-srun -u python  hp_search.py --model informer  --checkpoints ${1}  --data WTH --root_path ./  --attn qs --des 'Exp' --itr 5 --factor 5 --batch_size 16 --train_epochs 12
-srun -u python  hp_search.py --model informer  --checkpoints ${1} --data WTH --root_path ./   --attn lam --des 'Exp' --itr 5 --factor 5 --batch_size 16 --train_epochs 12
+srun -u python  hp_search.py --model informer  --data WTH --root_path ./  --attn qs --des 'Exp' --itr 5 --factor 5 --batch_size 16 --train_epochs 12
+srun -u python  hp_search.py --model informer  --data WTH --root_path ./   --attn lam --des 'Exp' --itr 5 --factor 5 --batch_size 16 --train_epochs 12
--- cluster/hp_tune_ettm1.sh
+++ cluster/hp_tune_ettm1.sh
@@ -8,5 +8,5 @@
 export PATH="/home/praphul/anaconda3/bin:$PATH"
 source activate informer-env
 
-srun -u python  hp_search.py --model informer  --checkpoints ${1}  --data ETTm1  --attn qs --des 'Exp' --itr 5 --factor 5 --batch_size 16 --train_epochs 12
-srun -u python  hp_search.py --model informer  --checkpoints ${1} --data ETTm1  --attn lam --des 'Exp' --itr 5 --factor 5 --batch_size 16 --train_epochs 12
+srun -u python  hp_search.py --model informer   --data ETTm1  --attn qs --des 'Exp' --itr 5 --factor 5 --batch_size 16 --train_epochs 12
+srun -u python  hp_search.py --model informer   --data ETTm1  --attn lam --des 'Exp' --itr 5 --factor 5 --batch_size 16 --train_epochs 12
